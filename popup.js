4300ac424af90511f0e5004470fb7bce1d82229e
JakeOJeff <realjakeojeff@gmail.com>
2025-06-17 01:42:56 +0530

    Checking POPUP

---
+++ popup.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => { // HTML fully Loaded
+    chrome.storage.local.get("osintData", (data) => {
+        if (data.osintData) {
+            document.getElementById("input").value = data.osintData;
+            runOSINT(data.osintData);
+        }
+    });
+    document.getElementById("lookup").addEventListener("click", () => {
+        const value = document.getElementById("input").value.trim();
+        runOSINT(value);
+    });
+});
+
+function runOSINT(input){
+
+    const resultDiv = document.getElementById("results");
+    resultDiv.textContent = "Scanning";
+
+
+    //IP CHECK
+    if (/^\d{1,3}(\.\d{1,3}){3}$/.test(input)) {
+        fetch(`https://ipapi.co/${input}/json/`)
+            .then(res => res.json())
+            .then(data => resultDiv.textContent = JSON.stringify(data, null, 2));
+    }
+
+    //EMAIL CHECK\
+    else if (input.includes("@")) {
+        resultDiv.textContent = "Checking for breaches... (UNAVAILABLE)";
+    }
+
+    //DOMAIN CHECK
+    else if(input.includes(".")){
+        fetch(`https://api.hackertarget.com/whois/?q=${input}`)
+            .then(res => res.text())
+            .then(data => resultDiv.textContent = data);
+    }
+
+    //ERROR fallback
+    else {
+        resultDiv.textContent = "UNKNOWN"
+    }
+}
+
