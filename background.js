81de433df7b3805a863432cfc27aa0a710287a31
JakeOJeff <realjakeojeff@gmail.com>
2025-06-17 01:20:00 +0530

    Added popup.html

--- background.js
+++ background.js
@@ -16,3 +16,15 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     chrome.storage.local.set({ osintData: selected });
     chrome.action.openPopup(); // Popup Open
 });
+
+// onInstalled → Runs once when extension is installed.
+// contextMenus.create → Adds a nenu item when right clicked on selected text
+// onClicked → When the menu is clicked, store the text and open the popup.
+
+/*
+    "icons": {
+        "16": "iconSS.png",
+        "48": "iconS.png",
+        "128": "icon.png"
+    }
+*/
